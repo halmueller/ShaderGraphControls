392c22ecaeac651c2dc4e2b8c28ad092818076c6
Hal Mueller <halmueller@gmail.com>
2024-08-05 15:53:06 -0700

    Tighten up parameter updates.

--- ShaderGraphControls/ViewModel.swift
+++ ShaderGraphControls/ViewModel.swift
@@ -13,23 +13,12 @@ import RealityKit
 final class ViewModel {
     var rootEntity: Entity? = nil
 
-    var red: Float = 1.0 {
-        didSet {
-            update(parameter: "red", newValue: red)
-        }
-    }
-    var green: Float = 1.0 {
-        didSet {
-            update(parameter: "green", newValue: green)
-        }
-    }
-    var blue: Float = 1.0 {
-        didSet {
-            update(parameter: "blue", newValue: blue)
-        }
-    }
+    var red: Float = 1.0
+    var green: Float = 1.0
+    var blue: Float = 1.0
     
     func updateParameters() {
+        print(#function, "****", rootEntity)
         // Using an Enum here would be cleaner. The parameter name strings must match the input nodes' names as defined in Pipeline.usda.
         update(parameter: "red", newValue: red)
         update(parameter: "green", newValue: green)
@@ -37,6 +26,7 @@ final class ViewModel {
     }
     
     func update(parameter: String, newValue: Float) {
+        print(#function, parameter, newValue)
         guard let cube = rootEntity?.findEntity(named: "Cube"),
               var modelComponent = cube.components[ModelComponent.self],
               var shaderGraphMaterial = modelComponent.materials.first
